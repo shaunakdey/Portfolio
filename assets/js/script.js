ffcb9e75fc4e938d609bdae376960b611398b118
Shaunak Dey <shaunak.dey7@gmail.com>
2018-07-30 08:02:06 +0530

    v1.1 of Portfolio
    Revamped home page and loads of optimizations

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,6 @@ $(document).ready(function(){
 
     $('#shadow-fiend').on('click', function(){
         new Howl({src: ['assets/sounds/collection.mp3']}).play();
-        $('.speech').css('opacity', '1');
-        setTimeout(() => {
-            $('.speech').css('opacity', '0');
-        }, 6000);
     });
     
     //Change flex-direction based on aspect-ratio
@@ -47,15 +43,52 @@ $(document).ready(function(){
         }
     });
 
-    //Load Particles
-    particlesJS.load('particles-js', 'assets/js/particle-system.json', function() {
-        console.log('callback - particles.js config loaded');
-    });
+    //Scramble and Order
+    var homeHiText = "Hi, I am Shaunak";
+    var hiInnerHTML = "";
+    var homeTitleText = "A Full Stack Web Developer";
+    var titleInnerHTML = "";
+    for(var i = 0; i < homeHiText.length; i++) {
+        hiInnerHTML += "<div class='scramble'><h2 style='font-size: 48px;'>" + (homeHiText.charAt(i) == ' ' ? '&nbsp' : homeHiText.charAt(i)) + "</h2></div>";
+    }
+    for(var i = 0; i < homeTitleText.length; i++) {
+        titleInnerHTML += "<div class='scramble'><h5 style='font-size: 32px;'>" + (homeTitleText.charAt(i) == ' ' ? '&nbsp' : homeTitleText.charAt(i)) + "</h5></div>";
+    }
+    $('.home-hi').html(hiInnerHTML);
+    $('.home-title').html(titleInnerHTML);
+    var scrambledLetters = $('.scramble');
+    console.log(scrambledLetters);
+    var scramble = function(){
+        for(var i = 0; i < scrambledLetters.length; i++) {
+            var style = "scale(" + (Math.random() * 0.75 + 0.5) + ") translate(" + (Math.random() * 1000 - 500) + "%, " + (Math.random() * 600 - 200) + "%)";
+            scrambledLetters[i].style.transform = style;
+        }
+        $('.home-mischief').css('opacity','0');
+        setTimeout(function(){
+            $('.home-mischief').css('zIndex','-1');
+        }, 550);
+        $('.home-swear').css('zIndex', '1').css('opacity', '1');
+        console.log('Scrambled');
+    };
+    var order = function(){
+        for(var i = 0; i < scrambledLetters.length; i++) {
+            scrambledLetters[i].style.transform = "scale(1) translate(0)";
+        }
+        $('.home-swear').css('opacity', '0');
+        setTimeout(function(){
+            $('.home-swear').css('zIndex','-1');
+        }, 550);
+        $('.home-mischief').css('zIndex', '1').css('opacity','1');
+        console.log("Ordered");
+    };
+    scramble();
+    $('.home-swear').click(order);
+    $('.home-mischief').click(scramble);
 
     // FullPage Code
     $('#fullpage').fullpage({
         licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-        sectionsColor: ['white', '#17202a', '#aeb6bf', 'white'],
+        sectionsColor: ['#121212', '#17202a', '#aeb6bf', 'white'],
         navigation: true,
         anchors: ['home', 'about', 'projects', 'contact'],
         menu: '#menu',
@@ -65,41 +98,26 @@ $(document).ready(function(){
         paddingBottom: '5%',
 
         afterLoad: function(anchorLink, index) {
-            if(index.index == 0) {
-                $('#home-hi').addClass('animated zoomIn').css('animation-delay', '0.5s');
-                $('.card').addClass('animated fadeInLeft');
-                var cards = $('.card');
-                for(var i = 0; i < cards.length; i++){
-                    cards[i].style.animationDelay = 0.8 + 0.2 * i + 's';
-                }
-                $('#home-title').addClass('animated zoomIn').css('animation-delay', '2.6s');
-                $('#mask').addClass('animated bounceInDown').css('animation-delay', '3s');
-                $('#rolling-text').addClass('animated zoomIn').css('animation-delay', '3.2s');
-                setTimeout(() => {
-                    $('.element').removeClass('animated');
-                }, 3000);
-            }
         },
         
         onLeave: function(index, nextIndex, direction) {
             if(nextIndex.index == 1){
-                //Change navigation color
-                $('#heading h1 a').toggleClass('change-color-white');
-                $('#heading a i').css('background-color', 'white');
-                $('#fp-nav ul li a span').css('background-color', 'white');
-
                 //Animate About Me Section
-                var animations = ['fadeInLeft', 'fadeInUp', 'fadeInDown', 'fadeInRight'];
-                var order = [3, 1, 2, 5, 4];
+                var animations = ['fadeInLeft', 'fadeInDown', 'fadeInRight', 'fadeInUp', 'fadeInLeft'];
                 $('.panel').addClass('animated');
                 var panels = $('.panel');
                 for(var i = 0; i < panels.length; i++) {
-                    panels[i].classList.add(animations[Math.floor(Math.random() * 4)]);
-                    panels[i].style.animationDelay = 0.2 * order[i] + 's';
+                    panels[i].classList.add(animations[i]);
+                    panels[i].style.animationDelay = 0.5 + 0.3 * i + 's';
                 }
             }
 
             else if(nextIndex.index == 2) {
+                //Change navigation color
+                $('#heading h1 a').css('color', '#000');
+                $('#heading a i').css('background-color', 'black');
+                $('#fp-nav ul li a span').css('background-color', 'black');
+
                 $('#projects-whoops').addClass('animated fadeInDown').css('animation-delay', '0.2s');
                 $('#projects-text').addClass('animated fadeInDown').css('animation-delay', '1s');
                 $('.link-github').addClass('animated lightSpeedIn').css('animation-delay', '2s');
@@ -115,10 +133,10 @@ $(document).ready(function(){
                 }
             }
 
-            if(index.index == 1){
-                $('#heading h1 a').toggleClass('change-color-white');
-                $('#heading a i').css('background-color', '#17202a');
-                $('#fp-nav ul li a span').css('background-color', '#17202a');
+            if((nextIndex.index == 1) || (nextIndex.index == 0)){
+                $('#heading h1 a').css('color', '#fff');
+                $('#heading a i').css('background-color', '#fff');
+                $('#fp-nav ul li a span').css('background-color', '#fff');
             }
         }
     });
